3b98583f9c319fb09c56317c308c9c2e2f0f2891
Alex Maskeny <maskeny@umich.edu>
2023-09-11 15:51:52 -0400

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Grace Charlotte Suber on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
